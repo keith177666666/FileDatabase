5e9c180f3f73a7453f32a41a0010cdb37883ea8d
keith17766666 <109770334+keith17766666@users.noreply.github.com>
2025-01-03 15:13:36 +0800

    adding more override method of initial()

--- src/main/java/dev/keith/AbstractFileDataBaseObserver.java
+++ src/main/java/dev/keith/AbstractFileDataBaseObserver.java
@@ -1,6 +1,17 @@
 package dev.keith;
 
+import dev.keith.data.StringData;
+
 public abstract class AbstractFileDataBaseObserver<K,
         S extends IDataBaseObserver.Serializer<K, StringData>>
         implements IDataBaseObserver<K, String, StringData, S> {
+    protected abstract static class Serializer<K,
+            THIS extends IDataBaseObserver.Serializer<K, StringData>,
+            O extends AbstractFileDataBaseObserver<K, THIS>>
+            implements IDataBaseObserver.Serializer<K, StringData> {
+        protected final O observer;
+        protected Serializer(O observer) {
+            this.observer = observer;
+        }
+    }
 }
